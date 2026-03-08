ede061cc12fa9126f4408a12162503d60c04925b
Student <ratpack-student@gmail.com>
2026-03-08 14:06:59 -0400

    added the new intake encoder -callum & anaya

--- src/main/include/HAL/IntakeHAL.h
+++ src/main/include/HAL/IntakeHAL.h
@@ -4,11 +4,12 @@
 #include "CanConfig.h"
 #include <rev/SparkMax.h>
 #include <MechanismConfig.h>
+#include <frc/Encoder.h>
 
 class IntakeHAL
 {
     public:
-        IntakeHAL() = default;
+        IntakeHAL();
         ~IntakeHAL() = default;
 
         void RunIntake(int direction); //TODO goog...le it once upon a time there was a cat and it googed... the end and buy my course on how to make money on temu.com -googing676767
@@ -21,6 +22,12 @@ class IntakeHAL
         rev::spark::SparkMax m_angleMotor{INTAKE_ANGLE_CAN_ID, rev::spark::SparkMax::MotorType::kBrushless};
         int m_intakeState = 0;
         double deadzone = 0.02;
+        double m_zeroPos;
+
+        frc::Encoder encoder = frc::Encoder(1,2,false);
+        // 	Encoder (int aChannel, int bChannel, bool reverseDirection=false, EncodingType encodingType=k4X)
+
+
 };
 
 
