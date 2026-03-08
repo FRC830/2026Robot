cce4afa06e559d1294a0f1b0fedff31c802c149a
TheSiddyBoi <sid.pandit04@gmail.com>
2026-03-08 17:21:27 -0400

    intake encoder change

--- src/main/cpp/HAL/IntakeHAL.cpp
+++ src/main/cpp/HAL/IntakeHAL.cpp
@@ -1,18 +1,28 @@
 #include "HAL/IntakeHAL.h"
 #include <frc/smartdashboard/SmartDashboard.h>
+
+// contructor: set the zero position of the encoder to the current value
+// abosulte encoder stuff below gets re
+
+// m_zeroPos = encoder.Get()
+IntakeHAL::IntakeHAL()
+{
+    m_zeroPos = encoder.Get();
+}
+
 void IntakeHAL::RunIntake(int direction)
 {
-    m_rollerMotor->Set(direction * ratbot::Intake::INTAKE_ROLLER_SPEED);
+    m_rollerMotor.Set(direction * ratbot::Intake::INTAKE_ROLLER_SPEED);
 }
 void IntakeHAL::MoveIntake(int direction)
 {
     m_angleMotor.Set(direction * ratbot::Intake::INTAKE_ANGLE_SPEED);
-    frc::SmartDashboard::PutNumber("IntakeAngle", m_angleMotor.GetAbsoluteEncoder().GetPosition());
+    frc::SmartDashboard::PutNumber("IntakeAngle", encoder.Get());
 }
 
 void IntakeHAL::SequenceDown()
 {
-    if (m_angleMotor.GetAbsoluteEncoder().GetPosition() < ratbot::Intake::DOWN_LOCATION + deadzone)
+    if (encoder.Get() < ratbot::Intake::DOWN_LOCATION + deadzone)
     {
         MoveIntake(-1);
     }
@@ -24,7 +34,7 @@ void IntakeHAL::SequenceDown()
 
 void IntakeHAL::SequenceStore()
 {
-    if (m_angleMotor.GetAbsoluteEncoder().GetPosition() > 0 + deadzone)
+    if (encoder.Get() > m_zeroPos + deadzone)
     {
         MoveIntake(1);
     }
--- src/main/include/HAL/IntakeHAL.h
+++ src/main/include/HAL/IntakeHAL.h
@@ -4,11 +4,12 @@
 #include "CanConfig.h"
 #include <rev/SparkMax.h>
 #include <MechanismConfig.h>
+#include <frc/Encoder.h>
 
 class IntakeHAL
 {
     public:
-        IntakeHAL() = default;
+        IntakeHAL();
         ~IntakeHAL() = default;
 
         void RunIntake(int direction); //TODO goog...le it once upon a time there was a cat and it googed... the end and buy my course on how to make money on temu.com -googing676767
@@ -17,10 +18,14 @@ class IntakeHAL
         void SequenceStore();
 
     private:
-        std::shared_ptr<ctre::phoenix6::hardware::TalonFX> m_rollerMotor = std::make_shared<ctre::phoenix6::hardware::TalonFX>(INTAKE_ROLLER_CAN_ID);
+        rev::spark::SparkMax m_rollerMotor{INTAKE_ROLLER_CAN_ID, rev::spark::SparkMax::MotorType::kBrushless};
         rev::spark::SparkMax m_angleMotor{INTAKE_ANGLE_CAN_ID, rev::spark::SparkMax::MotorType::kBrushless};
         int m_intakeState = 0;
         double deadzone = 0.02;
+        double m_zeroPos;
+
+        frc::Encoder encoder = frc::Encoder(1,2,false);
+        // 	Encoder (int aChannel, int bChannel, bool reverseDirection=false, EncodingType encodingType=k4X)
+
+
 };
-
-
