d2e46f4e6e001aab7d76d2203d300d49a979bbcf
TheCrudeOilBro <111211541+TheCrudeOilBro@users.noreply.github.com>
2026-03-11 11:15:12 -0400

    commented out intake moving cuz apparently we won't have an intake pivot

--- src/main/cpp/InputManager/IntakeManager.cpp
+++ src/main/cpp/InputManager/IntakeManager.cpp
@@ -5,11 +5,11 @@ void IntakeManager::HandleInput(RobotControlData &robotControlData)
 {
     if (robotControlData.intakeInput.intakeState)//true means up
     {
-        m_intakeHAL.SequenceStore();
+        //m_intakeHAL.SequenceStore();
     }
     if (!robotControlData.intakeInput.intakeState)
     {
-        m_intakeHAL.SequenceDown();
+        //m_intakeHAL.SequenceDown();
     }
     m_intakeHAL.RunIntake(robotControlData.intakeInput.intakeDirection);
     frc::SmartDashboard::PutNumber("IntakeAngle", m_intakeHAL.GetIntakeAngle());
