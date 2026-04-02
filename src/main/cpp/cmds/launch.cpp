4306f1209ed6768e3db356f5895691547cceb900
TheCrudeOilBro <111211541+TheCrudeOilBro@users.noreply.github.com>
2026-04-02 10:08:06 -0400

    fix launch cmd

--- src/main/cpp/cmds/launch.cpp
+++ src/main/cpp/cmds/launch.cpp
@@ -7,16 +7,21 @@ launch::launch(RobotControlData& data) : m_data(data)
 
 void launch::Initialize()
 {
-    m_data.launcherInput.disableLauncher = true;
-    m_data.launcherInput.indexerSpeeds = 0;
-    m_data.launcherInput.autoAim = false;
+    m_data.states.Launching = false;
+    m_data.states.Passing = false; // Ensure Passing is false when Launching is toggled
+    m_data.states.Intaking = false; // Ensure Intaking is false when Launching is toggled
+    m_data.states.Jam = false; // Ensure Jam is false when Launching is toggled
+    m_data.states.Defense = false; // Ensure Defense is false when Intaking is toggled
     m_timer.Restart();
 }
 
 void launch::Execute()
 {
-    m_data.launcherInput.launcherRPM = -3000;
-    m_data.states.Passing = true;
+    m_data.states.Launching = true;
+    m_data.states.Passing = false; // Ensure Passing is false when Launching is toggled
+    m_data.states.Intaking = false; // Ensure Intaking is false when Launching is toggled
+    m_data.states.Jam = false; // Ensure Jam is false when Launching is toggled
+    m_data.states.Defense = false; // Ensure Defense is false when Intaking is toggled
 }
 
 bool launch::IsFinished()
@@ -28,7 +33,5 @@ void launch::End(bool interrupted)
 {
 
     m_data.launcherInput.disableLauncher = true; //disable launcher
-    m_data.launcherInput.indexerSpeeds = 0;
-    m_data.launcherInput.autoAim = false;
-    m_data.states.Passing = false;
+    m_data.launcherInput.states.Launching = false;
 }
